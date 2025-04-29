9d7a328b955441b667d34a26fd6ccfacc4a12d25
Wuilliam Gavidia <zerpaw@gmail.com>
2025-04-28 22:28:46 -0500

    estilos css

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,10 +4,10 @@ toggle.addEventListener('change',(event)=> {
         let checked=event.target.checked;
         document.body.classList.toggle('dark');
         if (checked==true){
-         label_toogle.innerHTML = '<i class="fa-solid fa-sun"></i>';
+         label_toogle.innerHTML = '<i class="fa fa-sun-o" aria-hidden="true"></i>';
          label_toogle.style.color = "yellow";
         } else {
-            label_toogle.innerHTML = '<i class="fa-solid fa-moon"></i>';
+            label_toogle.innerHTML = '<i class="fa fa-moon-o" aria-hidden="true"></i>';
             label_toogle.style.color = "rebeccapurple";
         }
     })
